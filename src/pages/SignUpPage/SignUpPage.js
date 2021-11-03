ef0ddc6d1f9e6d7fcde3bc0eeb77153b49a24799
SthefanyLara <sthefany.gabriele@hotmail.com>
2021-11-03 19:30:05 -0300

    cadastro

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,13 +1,103 @@
 import React from "react"
+import useForm from '../../hooks/useForm';
+import { useHistory } from "react-router-dom";
+import LogoFood from '../../assets/logo-future-eats.png'
+import Back from '../../assets/back.png'
+import {PageContainer, ButtonContainer, Logo, TextStyle} from './styled'
+import { TextField, Button } from "@material-ui/core";
+import axios from 'axios';
+import { BASE_URL } from '../../constants/urls'
+import { goToLogin } from "../../routes/cordinator";
+import InputMask from 'react-input-mask' 
 
 
-SignUpPage = () =>
-{
-    return(
-        <div>
-            SignUpPage
-        </div>
-    )
-}
+const LoginPage = () => {
+    const [form, onChange, clear] = useForm({name:"", email:"", cpf:"", password: "", password: ""});
 
-export default SignUpPage
+    const history = useHistory ()
+    
+  const onSubmitForm = (event) => {
+    event.preventDefault()
+    SignUp()
+  }
+
+  const SignUp = (form, clear, history) => {
+    axios.post(`${BASE_URL}/signUp`, form)
+    .then((res) => console.log(res))
+      // localStorage.setItem("token", res.data.token)
+      // clear()
+      // goToSignUp(history)
+    // })
+    .catch((err) => console.log(err.response.data.message))
+  }
+
+    return (
+      <PageContainer>
+        <BackButton src ={Back}/>
+        <Logo src ={LogoFood}/>
+        <TextStyle> Cadastrar </TextStyle>
+        <form onSubmit={onSubmitForm}>
+        <TextField
+                placeholder = 'Name'
+                name={"name"}
+                value = {form.name}
+                onChange = {onChange}
+                required
+                label={"Name"}
+                variant={"outlined"}
+                fullWidth
+                margin={"normal"}
+          />
+          <TextField
+                placeholder = 'email@email.com'
+                name={"email"}
+                value = {form.email}
+                onChange = {onChange}
+                required
+                label={"E-mail"}
+                variant={"outlined"}
+                fullWidth
+                margin={"normal"}
+          />
+          < InputMask mask='999.999.999-99'
+                    type={"cpf"}
+                    name={"cpf"}
+                    value={form.cpf}
+                    onChange={onChange}
+                    placeholder="Cpf"
+                    required
+            />
+          <TextField
+                placeholder = "Minimun of 6 characters"
+                name={"password"}
+                value = {form.password}
+                onChange = {onChange}
+                required
+                label={"Password"}
+                variant={"outlined"}
+                margin={"normal"}
+                type={"Password"}
+                fullWidth
+          />
+            <TextField
+                placeholder = "Minimun of 6 characters"
+                name={"password"}
+                value = {form.password}
+                onChange = {onChange}
+                required
+                label={"confirm password"}
+                variant={"outlined"}
+                margin={"normal"}
+                type={"Password"}
+                fullWidth
+          />
+
+              
+          <ButtonContainer type={"submit"}> Create Account </ButtonContainer>
+        </form>
+ 
+      </PageContainer>
+    );
+  }
+  
+  export default LoginPage;
