2f2b1989b6314ce4ec2b8cd6aa39b8e7a33e31a8
SthefanyLara <sthefany.gabriele@hotmail.com>
2021-11-05 09:01:48 -0300

    cadastro-atualizadoo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import Router from "./routes/Router"
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import RegisterAddressPage from './pages/SignUpPage/RegisterAddressPage';
+import EditInfoPage from './pages/EditInfoPage/EditInfoPage'
 
 function App() {
   return (
     <div>
-      <RegisterAddressPage />
+      <Router />
 
     </div>
   )
