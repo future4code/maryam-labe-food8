8c9dd724d14a47ecd46539855be5013f2ac19690
Carina Santos Ferreira <carinasf14@gmail.com>
2021-11-05 20:07:31 -0300

    Página de restaurante atualizada

--- src/pages/RestaurantPage/RestaurantPage.js
+++ src/pages/RestaurantPage/RestaurantPage.js
@@ -1,35 +1,59 @@
-import React from "react";
-import { BackButton, Header, Title, Product, Name, Div, Description, P, Category } from './styled'
-import Back from '../../assets/back.png'
-import NewBurguer from '../../assets/burguer.png'
-import ProductCard from '../../components/ProductCard/ProductCard'
+import React, { useEffect } from 'react'
+import { BackButton, Header, Title, Product, Name, Div, Description, P, Category } from './styled';
+import Back from '../../assets/back.png';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import useProtectedPage from '../../hooks/useProtectedPage';
+import useRequestData from '../../hooks/useRequestData';
+import {BASE_URL} from '../../constants/urls';
 
-const RestaurantPage = () =>
-{
+
+const RestaurantPage = () =>{
+    useProtectedPage()
+
+    const restaurant = useRequestData([], `${BASE_URL}/restaurants/2`)[0]
+    console.log(restaurant)
+    
+    // const getDetails = restaurant.map((detail) => { 
+    //     return (
+    //       <ProductCard
+    //         key={detail.id}
+    //         name={detail.name}
+    //         description={detail.description}
+    //         price={detail.price}
+    //       />
+    
+    //     )
+    //   })
+
+
     return(
         <div>
             <Header>
                 <BackButton>
-                    <img src = {Back}/>
+                    <img src = {Back} alt="Símbolo que indica o ato de voltar"/>
                 </BackButton>
                 <Title> Restaurante</Title>
-                {/* <p></p> */}
             </Header>
-            <Product src={NewBurguer}/>
-            <Name>Nome do restaurante</Name>
+
+            <Product src={restaurant.restaurant && restaurant.restaurant.logoUrl} alt="Logo do restaurante"/>
+            <Name>{restaurant.restaurant && restaurant.restaurant.name}</Name>
             <div>
-                <Description>Produto</Description>
+                <Description>{restaurant.restaurant && restaurant.restaurant.category}</Description>
                 <Div>
-                    <Description>XX-XX min</Description>
-                    <Description>Frete R$XX,XX</Description>
+                    <Description>{restaurant.restaurant && restaurant.restaurant.deliveryTime} min</Description>
+                    <Description>R${restaurant.restaurant && restaurant.restaurant.shipping.toFixed(2)}</Description>
                 </Div>
-                <P>Rua do estabelecimento</P>
+                <P>{restaurant.restaurant && restaurant.restaurant.address}</P>
             </div>
             <br/>
+
             <Category> Principais </Category>
+           
             <ProductCard/>
             <Category> Acompanhamentos </Category>
             <ProductCard/>
+           
+       
             
 
         </div>
